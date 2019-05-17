1fa554acef1ed13598e10a0c95ee29e0425baa3e
mvernon27 <mvernon2705@gmail.com>
2019-05-17 14:44:51 -0400

    Added Menu Interface

--- Program.cs
+++ Program.cs
@@ -6,7 +6,35 @@ namespace VirtualPetTeam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string startExitDirections = "";
+            do
+            { 
+            Console.WriteLine("1 = start: 2 = directions: 3 = exit");
+            startExitDirections = Console.ReadLine();
+
+                if (startExitDirections == "2")
+                {
+                    Console.WriteLine("directions");
+                }
+
+            if (startExitDirections == "3")
+            {
+                Environment.Exit(0);
+            }
+            }
+            while (startExitDirections != "1");
+
+            if (startExitDirections != "3" && startExitDirections != "2")
+                {
+                    Console.WriteLine("hello");
+
+
+
+
+
+
+            }
+
         }
     }
 }
