c638ae7294517298b3fb5f8d87bbb87887fe2d31
AJFerguson123 <roverrecovery1@gmail.com>
2019-05-20 15:32:16 -0400

    addedmenu

--- Program.cs
+++ Program.cs
@@ -6,23 +6,62 @@ namespace VirtualPetTeam
     {
         static void Main(string[] args)
         {
-            string startExit = "";
+            string menuChoice = "";
             bool playing = true;
             do
             {
-                Console.WriteLine("1 = start: 2 = exit");
-                startExit = Console.ReadLine();
+                Console.WriteLine("1 = start: 2 = add pet: 3 = exit");
+                menuChoice = Console.ReadLine();
 
-                if (startExit == "2")
+
+                switch (menuChoice)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        
+                        Console.WriteLine("What would you like to name your pet?");
+                        string Name = Console.ReadLine();
+                        Console.WriteLine("What would you like its species?");
+                        string Species = Console.ReadLine();
+
+                        break;
+                    case "3":
+                        Environment.Exit(0);
+                        break;
+
+                    
+                
+                        
+
+
+
+
+
+                }
+
+
+/*
+
+                if (menuChoice == "3")
                 {
                     playing = false;
                 }
+                if (menuChoice == "2") 
+*/
+                    
+            }
 
-            } while (playing);
             
+                while (playing) ;
+
+            
+
+            
+
             Cat fatCat = new Cat("Fatcat", "Tabbycat");
                 
-            Console.WriteLine("Goodbye");
+            Console.WriteLine("Good bye player.");
         }
     }
 }
