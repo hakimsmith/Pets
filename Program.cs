becc4d76ba87a596080bbaf1ad8d24c34a578561
AJFerguson123 <roverrecovery1@gmail.com>
2019-05-20 15:59:05 -0400

    addedpetinfo

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace VirtualPetTeam
             bool playing = true;
             do
             {
-                Console.WriteLine("1 = start: 2 = add pet: 3 = exit");
+                Console.WriteLine("1 = start: 2 = add pet: 3 = pet info: 4 = exit:");
                 menuChoice = Console.ReadLine();
 
 
@@ -19,14 +19,16 @@ namespace VirtualPetTeam
                     case "1":
                         break;
                     case "2":
-                        
+                        Cat bigCat = new Cat("no name", "non species");
                         Console.WriteLine("What would you like to name your pet?");
-                        string Name = Console.ReadLine();
+                        bigCat.Name = Console.ReadLine();
                         Console.WriteLine("What would you like its species?");
-                        string Species = Console.ReadLine();
-
+                        bigCat.Species = Console.ReadLine();
                         break;
                     case "3":
+                        Console.WriteLine(" Your pet's name is" + bigCat.Name + ", the species" + bigCat.Species );
+                        break;
+                    case "4":
                         Environment.Exit(0);
                         break;
 
