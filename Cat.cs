9511e0af9f7cb7b2a43e83b3559f943be6656300
mvernon27 <mvernon2705@gmail.com>
2019-05-20 22:17:27 -0400

    Added methods/Variables

--- Cat.cs
+++ Cat.cs
@@ -12,7 +12,6 @@ namespace VirtualPetTeam
         public int Boredom { get; set; }
         public int Health { get; set; }
 
-
         public Cat(string name, string species, int hunger, int boredom, int health)
         {
             Name = name;
@@ -21,10 +20,12 @@ namespace VirtualPetTeam
             Boredom = boredom;
             Health = health;
         }
+
         public void Greet()
         {
             Console.WriteLine("I'm " + Name + "! My species is " + Species + ".");
         }
+
         public void Status()
         {
             Console.WriteLine(Name + "'s status is :");
@@ -32,19 +33,61 @@ namespace VirtualPetTeam
             Console.WriteLine("Bordeom: " + Boredom + "/20");
             Console.WriteLine("Health: " + Health + "/20");
         }
+
         public void Feed()
         {
             Hunger = (Hunger + 2);
         }
+
         public void Play()
         {
             Boredom = (Boredom + 3);
             Hunger = (Hunger - 1);
             Health = (Health - 1);
         }
+
         public void Doctor()
         {
             Health = (Health + 2);
+        }
+
+        public void Interact()
+        {
+            bool interacting = true;
+            string interact = "";
+            do
+            {
+                Console.WriteLine("1 = Feed: 2 = Play: 3 = Take your pet to the Vet: 4 = Quit Interacting");
+                interact = Console.ReadLine();
+                switch (interact)
+                {
+                    case "1":
+                        Feed();
+                        break;
+                    case "2":
+                        Play();
+                        break;
+                    case "3":
+                        Doctor();
+                        break;
+                    case "4":
+                        {
+                            interacting = false;
+                        }
+                        while (interacting) ;
+                        break;
+                }
+            }
+
+            while (interacting);
+        }
+
+        public void Addpet()
+        {
+            Console.WriteLine("What would you like to name your pet?");
+            Name = Console.ReadLine();
+            Console.WriteLine("What would you like its species?");
+            Species = Console.ReadLine();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,43 +13,14 @@ namespace VirtualPetTeam
             {
                 Console.WriteLine("1 = Interact: 2 = Add Pet: 3 = Pet Info: 4 = Exit: 5 = Pet Status:");
                 menuChoice = Console.ReadLine();
-
-
                 switch (menuChoice)
                 { 
                     case "1":
-                        bool interacting = true;
-                        string interact = ""; 
-                        do
-                        {
-                            Console.WriteLine("1 = Feed: 2 = Play: 3 = Take your pet to the Vet: 4 = Quit Interacting");
-                            interact = Console.ReadLine();
-                            switch (interact)
-                            {
-                                case "1":
-                                    newCat.Feed();
-                                    break;
-                                case "2":
-                                    newCat.Play();
-                                    break;
-                                case "3":
-                                    newCat.Doctor();
-                                    break;
-                                case "4":
-                                    {
-                                        interacting = false;
-                                    }
-                                    break;
-                            }
-                        }
-                        while (interacting);
+                        newCat.Interact();  
                         break;
 
                     case "2":
-                        Console.WriteLine("What would you like to name your pet?");
-                        newCat.Name = Console.ReadLine();
-                        Console.WriteLine("What would you like its species?");
-                        newCat.Species = Console.ReadLine();
+                        newCat.Addpet();
                         break;
                        
                     case "3":
@@ -65,8 +36,7 @@ namespace VirtualPetTeam
                         break;
                 }
             }
-                while (playing) ;
-            Console.WriteLine("Good-bye player.");
+            while (playing);
         } 
     }
 }
