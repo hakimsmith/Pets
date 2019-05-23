b89a63fcb12e0d91c043ca9c12d8d1cbbbfba65e
Hakim Smith <hakim.smith19@yahoo.com>
2019-05-23 07:25:46 -0400

    Added new menu class

---
+++ Menus.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualPetTeam
+{
+    class Menus
+    {
+        public void Mainmenu()
+        {
+            string menuChoice = "";
+            bool playing = true;
+            Cat newCat = new Cat("no name", "no species");
+            do
+            {
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadLine();
+                Console.WriteLine(" 1 = Interact: \n 2 = Add Pet:\n 3 = Pet Info:\n 4 = Pet Status:\n 5 = Exit:");
+                menuChoice = Console.ReadLine();
+                switch (menuChoice)
+                {
+                    case "1":
+                        newCat.Interact();
+                        break;
+
+                    case "2":
+                        newCat.AddPet();
+                        break;
+
+                    case "3":
+                        newCat.PetInfo();
+                        break;
+
+                    case "4":
+                        newCat.PetStatus();
+                        break;
+
+                    case "5":
+                        playing = false;
+                        break;
+                }
+            }
+            while (playing);
+        }
+    }
+
+}
+            
+
--- Program.cs
+++ Program.cs
@@ -6,40 +6,8 @@ namespace VirtualPetTeam
     {
         static void Main(string[] args)
         {
-            Cat newCat = new Cat("no name", "no species");
-            string menuChoice = "";
-            bool playing = true;
             Console.WriteLine("Welcome to the Pet Shelter.");
-            do
-            {
-                Console.WriteLine("Press any key to continue.");
-                Console.ReadLine();
-                Console.WriteLine(" 1 = Interact: \n 2 = Add Pet:\n 3 = Pet Info:\n 4 = Pet Status:\n 5 = Exit:");
-                menuChoice = Console.ReadLine();
-                switch (menuChoice)
-                { 
-                    case "1":
-                        newCat.Interact();  
-                        break;
 
-                    case "2":
-                        newCat.AddPet();
-                        break;
-                       
-                    case "3":
-                        newCat.PetInfo(); 
-                        break;
-
-                    case "4":
-                        newCat.PetStatus();
-                        break;
-
-                    case "5":
-                        playing = false;
-                        break;
-                }
-            }
-            while (playing);
         } 
     }
 }
